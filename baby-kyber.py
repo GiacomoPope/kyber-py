077df6054c0791b713cafa408901289d58a5c190
jack4818 <giacomopope@gmail.com>
2022-08-22 12:58:37 +0100

    Clean up baby kyber impl.

--- baby-kyber.py
+++ baby-kyber.py
@@ -1,33 +1,29 @@
+"""
+This is a toy implementation of Kyber which follows
+https://cryptopedia.dev/posts/kyber/
+
+As the polynomials are given precisely in the blog post
+all calculations are done by calling `polynomials` and
+`modules` rather than `Kyber` itself.
+"""
+
 from polynomials import *
 from modules import *
 
-def round_up(x):
-    """
-    Round x.5 up always
-    """
-    return int(x + .5)
-
-def closer_to(x, y):
-    return abs(x - y) < (y/2)
-
-def poly_from_integer(m):
-    m_bin = list(map(int, list(bin(m)[2:][::-1])))
-    m_poly = R(m_bin)
-    m_scale = round_up(m_poly.parent.q / 2)
-    return m_scale * m_poly
-
-def integer_from_poly(p):
-    middle = round_up(p.parent.q / 2)
-    binary = ["1" if closer_to(x,middle) else "0" for x in p.coeffs]
-    return int("".join(binary[::-1]), 2)
-
 def keygen():
     # Randomness fixed for example
+    # Generate a secret key which
+    # is a vector with elements 
+    # pulled from a centred
+    # binomial distribution
     s0 = R([0,1,-1,-1])
     s1 = R([0,-1,0,-1])
     s = M([s0,s1]).transpose()
 
     # Randomness fixed for example
+    # Generate a 2x2 matrix with 
+    # elements taken randomly from
+    # R_q
     A00 = R([11,16,16,6])
     A01 = R([3,6,4,9])
     A10 = R([1,10,3,5])
@@ -35,47 +31,79 @@ def keygen():
     A = M([[A00, A01],[A10, A11]])
 
     # Randomness fixed for example
+    # gen random vector `e` from
+    # binomial distribution
     e0 = R([0,0,1,0])
     e1 = R([0,-1,1,0])
     e = M([e0,e1]).transpose()
 
+    # Compute `t` from example
     t = A @ s + e
+    
+    # Check against blogpost
+    assert t == M([R([7,0,15,16]),R([6,11,12,10])]).transpose()
     return (A, t), s
 
 def encrypt(m, public_key):
-    # randomness fixed for
-    # this example...
-    # gen r
+    # randomness fixed for example
+    # gen random vector `r` from
+    # binomial distribution
     r0 = R([0,0,1,-1]) 
     r1 = R([-1,0,1,1])
     r = M([r0, r1]).transpose()
-    # gen e1, e2
+    
+    # randomness fixed for example
+    # gen random vector `e_1` from
+    # binomial distribution
     e_10 = R([0,1,1,0])
     e_11 = R([0,0,1,0])
     e_1 = M([e_10, e_11]).transpose()
+    
+    # randomness fixed for example
+    # gen random polynomial `e_2` from
+    # binomial distribution
     e_2 = R([0,0,-1,-1])
 
     A, t = public_key
-    encoded_message = poly_from_integer(m)
+    poly_m = R.decode(m).decompress(1)
+    # Check against blogpost
+    assert poly_m == R([9,9,0,9])
+    
     u = A.transpose() @ r + e_1
-    v = (t.transpose() @ r)[0][0] + e_2 + encoded_message
+    # Check against blogpost
+    assert u == M([R([3,10,11,11]), R([11,13,4,4])]).transpose()
+    
+    # Typo in blog post, we need to use
+    # `- m` rather than `+ m` to make values match
+    v = (t.transpose() @ r)[0][0] + e_2 - poly_m  
+    assert v == R([15, 8 , 6, 7])
     return u, v
 
-def decrypt(u, v, private_key):
-    s = private_key
+def decrypt(u, v, s):
     m_n = v - (s.transpose() @ u)[0][0]
-    return integer_from_poly(m_n)
+    # Check against blogpost
+    assert m_n == R([5,7,14,7])
+    # Check against blogpost
+    m_n_reduced = m_n.compress(1)
+    assert m_n_reduced == R([1,1,0,1])
+    return m_n_reduced.encode(l=2)
     
 if __name__ == '__main__':
-    # for m in range(3,15):
-    #     R = PolynomialRing(17, 4)
-    #     M = Module(R)
-    #     pub, priv = keygen()
-    #     u, v = encrypt(m, pub)
-    #     n = decrypt(u, v, priv)
-    #     assert n == m, f"{n,m}"
-        
-    R = PolynomialRing(17, 16)
+    R = PolynomialRing(17, 4)
     M = Module(R)
+    # Our polynomial encoding follows Kyber spec
+    # So we encode the byte `b'\xa2'` to get the
+    # polynomial from the blog post
+    # >>> R.decode(bytes([162]))
+    # 1 + x + x^3
+    m = bytes([162])
+    assert R.decode(m) == R([1,1,0,1]) 
+    # Generate keypair
+    pub, priv = keygen()
+    # Encrypt message
+    u, v = encrypt(m, pub)
+    # Decrypt message
+    n = decrypt(u, v, priv)
+    assert n == m
 
 
