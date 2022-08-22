6115aa35efa60cd47aa8c2dcd0e54e009d9c95dd
jack4818 <giacomopope@gmail.com>
2022-08-22 22:29:31 +0100

    Include internal consistency checks while KAT tests are being developed

---
+++ test_kyber.py
@@ -0,0 +1,44 @@
+import unittest
+from kyber import *
+
+class TestKyber(unittest.TestCase):
+    """
+    Test Kyber levels for internal
+    consistency by generating keypairs
+    and shared secrets.
+    """
+    def test_kyber512(self):
+        for _ in range(20):
+            pk, sk = Kyber512.keygen()
+            for _ in range(50):
+                c, key = Kyber512.encrypt(pk)
+                _key = Kyber512.decrypt(c, sk)
+                self.assertEqual(key, _key)
+    
+    def test_kyber768(self):
+        for _ in range(20):
+            pk, sk = Kyber768.keygen()
+            for _ in range(50):
+                c, key = Kyber768.encrypt(pk)
+                _key = Kyber768.decrypt(c, sk)
+                self.assertEqual(key, _key)
+                
+    def test_kyber1024(self):
+        for _ in range(20):
+            pk, sk = Kyber1024.keygen()
+            for _ in range(50):
+                c, key = Kyber1024.encrypt(pk)
+                _key = Kyber1024.decrypt(c, sk)
+                self.assertEqual(key, _key)
+    
+if __name__ == '__main__':
+    unittest.main()
+    """
+    output:
+        
+    ...
+    ----------------------------------------------------------------------
+    Ran 3 tests in 134.541s
+    
+    OK
+    """
