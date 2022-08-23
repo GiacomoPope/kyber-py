c0bffaeedf5f6bc5441c0e0fbb655814dcbe55d1
jack4818 <giacomopope@gmail.com>
2022-08-23 23:26:20 +0100

    Add unit test for KATs

--- test_kyber.py
+++ test_kyber.py
@@ -1,6 +1,19 @@
 import unittest
-from kyber import *
+from kyber import Kyber512, Kyber768, Kyber1024
 
+def parse_kat_data(data):
+    parsed_data = {}
+    count_blocks = data.split('\n\n')
+    for block in count_blocks[1:-1]:
+        block_data = block.split('\n')
+        count, seed, pk, sk, ct, ss = [line.split(" = ")[-1] for line in block_data]
+        parsed_data[count] = {
+            "sk": bytes.fromhex(sk),
+            "ct": bytes.fromhex(ct),
+            "ss": bytes.fromhex(ss),   
+        }
+    return parsed_data
+    
 class TestKyber(unittest.TestCase):
     """
     Test Kyber levels for internal
@@ -32,14 +45,37 @@ class TestKyber(unittest.TestCase):
                 _key = Kyber1024.decrypt(c, sk)
                 self.assertEqual(key, _key)
     
+    
+class TestKnownTestValues(unittest.TestCase):    
+    def test_kyber_512_known_answer(self):
+        with open("assets/PQCkemKAT_1632.rsp") as f:
+            kat_data_512 = f.read()
+            parsed_data = parse_kat_data(kat_data_512)
+            
+            for data in parsed_data.values():
+                sk, ct, ss = data.values()
+                _ss = Kyber512.decrypt(ct, sk)
+                self.assertEqual(ss, _ss)
+                
+    def test_kyber_768_known_answer(self):
+        with open("assets/PQCkemKAT_2400.rsp") as f:
+            kat_data_768 = f.read()
+            parsed_data = parse_kat_data(kat_data_768)
+            
+            for data in parsed_data.values():
+                sk, ct, ss = data.values()
+                _ss = Kyber768.decrypt(ct, sk)
+                self.assertEqual(ss, _ss)
+                
+    def test_kyber_1024_known_answer(self):
+        with open("assets/PQCkemKAT_3168.rsp") as f:
+            kat_data_1024 = f.read()
+            parsed_data = parse_kat_data(kat_data_1024)
+            
+            for data in parsed_data.values():
+                sk, ct, ss = data.values()
+                _ss = Kyber1024.decrypt(ct, sk)
+                self.assertEqual(ss, _ss)
+
 if __name__ == '__main__':
     unittest.main()
-    """
-    output:
-        
-    ...
-    ----------------------------------------------------------------------
-    Ran 3 tests in 134.541s
-    
-    OK
-    """
