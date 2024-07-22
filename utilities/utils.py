d404f9ea85cc32d2a88d4db5a37176cacfff4708
Giacomo Pope <giacomopope@gmail.com>
2024-07-23 00:02:29 +0100

    add a constant time select for decaps

--- utilities/utils.py
+++ utilities/utils.py
@@ -25,4 +25,18 @@ def xor_bytes(a, b):
     XOR two byte arrays, assume that they are
     of the same length
     """
+    assert len(a) == len(b)
     return bytes(a ^ b for a, b in zip(a, b))
+
+
+def select_bytes(a, b, cond):
+    """
+    Select between the bytes a or b depending
+    on whether cond is False or True
+    """
+    assert len(a) == len(b)
+    out = [0] * len(a)
+    cw = -cond % 256
+    for i in range(len(a)):
+        out[i] = a[i] ^ (cw & (a[i] ^ b[i]))
+    return bytes(out)
